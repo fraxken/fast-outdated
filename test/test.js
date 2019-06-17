3f9b7db12aa72482e287ff7e622e2b0348ce67d9
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-06-17 22:42:13 +0200

    test: improve by comparing with npm outdated cmd

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 // Require Node.js Dependencies
 const { join } = require("path");
+const { spawnSync } = require("child_process");
 
 // Require Third-party Dependencies
 const japa = require("japa");
@@ -8,11 +9,23 @@ const is = require("@slimio/is");
 // Require Internal Dependencies
 const outdated = require("../");
 
+// CONSTANTS
+const EXEC_SUFFIX = process.platform === "win32";
+
 japa("exported must be a function", (assert) => {
     assert.isTrue(is.asyncFunction(outdated), "outdated must be a Asynchronous Function");
 });
 
 japa("get members of current project", async(assert) => {
-    const deps = await outdated(join(__dirname, ".."));
-    assert.isTrue(is.plainObject(deps));
-});
+    const cwd = join(__dirname, "..");
+    const { stdout } = spawnSync(`npm${EXEC_SUFFIX ? ".cmd" : ""}`, ["outdated", "--json"], {
+        cwd
+    });
+    const str = stdout.toString().trim();
+    const json = str.length === 0 ? {} : JSON.parse(str);
+
+    const deps = await outdated(cwd, { devDependencies: true });
+    assert.isTrue(is.plainObject(deps), "deps must be plainObject");
+    assert.deepEqual(deps, json, "npm outdated and deps must be equal!");
+}).timeout(10000);
+
